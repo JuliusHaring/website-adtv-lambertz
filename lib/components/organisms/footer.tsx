71b137f4bf9264828339607d611181ad40726cd2
Julius Haring <info@juliusharing.com>
2025-05-05 12:54:09 +0200

    style: restyle footer

--- lib/components/organisms/footer.tsx
+++ lib/components/organisms/footer.tsx
@@ -1,18 +1,20 @@
 export default function Footer() {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
-      <div>
-        <span className="footer-title">Tanzschule</span>
-        <a className="link link-hover">Über uns</a>
-      </div>
-      <div>
-        <span className="footer-title">Rechtliches</span>
-        <a className="link link-hover" href="/footer/impressum">
-          Impressum
-        </a>
-        <a className="link link-hover" href="/footer/datenschutz">
-          Datenschutz
-        </a>
+    <footer className="bg-base-200 text-base-content px-10 py-8">
+      <div className="flex flex-col md:flex-row justify-between gap-8 max-w-4xl mx-auto w-full">
+        <div className="flex flex-col space-y-2">
+          <span className="footer-title">Tanzschule</span>
+          <a className="link link-hover">Über uns</a>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <span className="footer-title">Rechtliches</span>
+          <a className="link link-hover" href="/footer/impressum">
+            Impressum
+          </a>
+          <a className="link link-hover" href="/footer/datenschutz">
+            Datenschutz
+          </a>
+        </div>
       </div>
     </footer>
   );
