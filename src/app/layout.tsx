b432040c49dce8fb65613d97bd1e8adbed96197c
Julius Haring <info@juliusharing.com>
2025-05-13 13:19:44 +0200

    style: improve ipad padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
         <div className="relative z-10 min-h-screen flex flex-col">
           <Header />
           <ColorBar />
-          <main className="flex-grow px-4 md:px-40 py-6">
+          <main className="flex-grow px-4 lg:px-40 py-6">
             <div className="min-h-[calc(100vh-theme(spacing.24))]flex items-center justify-center">
               <Card hover={false} className="shadow-xl">
                 {children}
